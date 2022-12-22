b7b176dfd4fb30065774d5ddb5fb8083efa863ee
codigowebmp <103782811+codigowebmp@users.noreply.github.com>
2022-12-22 13:38:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
